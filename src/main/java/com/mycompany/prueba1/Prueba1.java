c98f94ab695bab7186d256855078cbb769aba660
TheoxJhonny <j.fuentealba.contacto@gmail.com>
2025-09-08 19:59:06 -0300

    init project

---
+++ src/main/java/com/mycompany/prueba1/Prueba1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba1;
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        String mensaje = "Este e sun mensaje";
+        System.out.println(mensaje);
+        
+    }
+    
+}
