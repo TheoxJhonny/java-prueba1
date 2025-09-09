8a29a6f18a6004b627c6abdc08e118d0347a2c0a
TheoxJhonny <j.fuentealba.contacto@gmail.com>
2025-09-08 21:40:27 -0300

    Se añadió Usuario y Alumno

--- src/main/java/com/mycompany/prueba1/Prueba1.java
+++ src/main/java/com/mycompany/prueba1/Prueba1.java
@@ -17,6 +17,30 @@ public class Prueba1 {
         String mensaje = "Este es un mensaje";
         System.out.println(mensaje);
         
+         
+    Usuario user1 = new Usuario("Jonathan", "Fuentealba", (byte)35, false, "Chilena"); 
+    Usuario user2 = new Usuario("Marcelo", "Concha", (byte)25, true, "Chilena"); 
+    Usuario user3 = new Usuario("Juan", "Castolo", (byte)40, false, "Haitiana"); 
+    Usuario user4 = new Usuario("Marco", "Aurelio", (byte)19, true, "Griega"); 
+    Usuario user5 = new Usuario("Javiera", "Mena", (byte)24, false, "Chilena"); 
+    
+    System.out.println(user1.toString());
+    System.out.println(user2.toString());
+    user2.setNombre("Ramiro");
+    System.out.println(user2.toString());
+    
+    Alumno alumno = new Alumno(6, 3, 5, "Jonathan", "Fuentealba", (byte)35, true, "Aleman");
+    alumno.saludar();
+    
+    
+        
     }
     
-}
+    //instanciar o invocar
+    //Usuario user1 = new Usuario();
+   
+    
+            
+        
+            
+  }
--- src/main/java/com/mycompany/prueba1/Usuario.java
+++ src/main/java/com/mycompany/prueba1/Usuario.java
@@ -9,5 +9,79 @@ package com.mycompany.prueba1;
  * @author Santo Tomas
  */
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private byte edad;
+    
+    private boolean esBecado;
+    private String nacionalidad;
+
+    public Usuario() {
+    }
+    
+
+    public Usuario(String nombre, String apellido, byte edad, boolean esBecado, String nacionalidad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.esBecado = esBecado;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public byte getEdad() {
+        return edad;
+    }
+
+    public void setEdad(byte edad) {
+        this.edad = edad;
+    }
+
+    public boolean isEsBecado() {
+        return esBecado;
+    }
+
+    public void setEsBecado(boolean esBecado) {
+        this.esBecado = esBecado;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", esBecado=" + esBecado + ", nacionalidad=" + nacionalidad + '}';
+    }
+    
+    
+    public void saludar(){
+        System.out.println("Mi nombre es " + nombre + " y soy usuario." );
+    }
+    
+    public String saludar2() {
+        return "Mi nombre es " + nombre + " y soy usuario.";
+    }
     
 }
+
+
